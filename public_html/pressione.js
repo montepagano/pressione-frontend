d7a679c3a7c653781787dd7daa15c4162d9037f5
utente <utente@NBCHIWYL0013>
2017-02-07 17:31:33 +0100

    Esercizio su Angular, basebase

--- public_html/pressione.js
+++ public_html/pressione.js
@@ -1,12 +1,35 @@
 app.controller('PressioneController', ['$http',function ($http) {
 var vm= this;
-vm.test="test";
-vm.init=function(){
-    $http.get("http://localhost/pa/elenco.php")
-            .success(function(data){console.log(data);})
-            .error(function(data){console.log("error:");console.log(data);});
+    vm.init=function(){
+        vm.test="test";
+        $http.get("http://localhost/pa/elenco.php")
+                .then(function(response){
+                        vm.response=response.data;
+                    },vm.response);
+            
 };
+    vm.init();
+    vm.delete=function(id){
+        $http.get("http://localhost/pa/elenco.php?act=del&id="+id)
+                .then(function(response){
+                        vm.response=response.data;
+                    },vm.response);
+    };
+    
+    vm.create=function(data){
+        
+       $http({
+           method:"POST",
+           url:"http://localhost/pa/elenco.php",
+           headers:{
+               'Content-Type':application/json
+           },
+           data:data
+       })
+            .then(function(response){
+                     vm.response=response.data;
+                 },vm.response);
+      
+    };
 
-vm.init();
 }]);
-
